5f975370ff53bade4c879e0b052125adc1fd21ce
petrSchreiber <petrschreiber@gmail.com>
2019-08-08 08:36:23 +0200

    simpler coloring

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,29 @@
 use std::env;
+use std::io::Write;
 use std::process::exit;
 
-use ansi_term;
-use colored::*;
+use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 
 mod compile;
 mod thinbasic_script;
 mod tokenizer;
 
-fn main() {
-    if cfg!(windows) && ansi_term::enable_ansi_support().is_err() {
-        colored::control::set_override(false);
-    }
+fn print_color(text: &str, color: Color) {
+    let mut stdout = StandardStream::stdout(ColorChoice::Always);
+    stdout
+        .set_color(ColorSpec::new().set_fg(Some(color)).set_intense(true))
+        .unwrap();
+    write!(&mut stdout, "{}", text).unwrap();
+    stdout
+        .set_color(ColorSpec::new().set_fg(Some(Color::White)).set_intense(false))
+        .unwrap();
+}
 
+fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
-        println!(
-            "{} Please supply thinBasic script file name as parameter",
-            "input error:".red().bold()
-        );
+        print_color("input error: ", Color::Red);
+        println!("Please supply thinBasic script file name as parameter");
         exit(1);
     }
 
@@ -28,7 +33,8 @@ fn main() {
     let mut code = match thinbasic_script::Code::new(main_file_name) {
         Ok(c) => c,
         Err(e) => {
-            println!("{}: {}", "error:".red().bold(), e);
+            print_color("error: ", Color::Red);
+            println!("{}", e);
             exit(1)
         }
     };
@@ -40,7 +46,9 @@ fn main() {
             Ok(()) => (),
             Err(v) => {
                 issues_found += 1;
-                println!("Line {:>2}:{:>2} {}", v.line, v.pos, v.summary.red().bold())
+                print!("Line {:>2}:{:>2} - ", v.line, v.pos);
+                print_color(&v.summary, Color::Red);
+                println!();
             }
         };
     } else {
@@ -48,15 +56,12 @@ fn main() {
     }
 
     println!("\n----------------------------------------");
-    print!("{}", "Analysis finished, ".white().bold());
+    print!("Analysis finished: ");
     if issues_found > 0 {
-        println!(
-            "{} {}",
-            issues_found.to_string().red().bold(),
-            "issue(s) found".red().bold()
-        );
+        print_color(&format!("{}", issues_found), Color::Red);
+        print_color(" issue(s) found\n", Color::Red);
     } else {
-        println!("{}", "no issues found".green().bold());
+        print_color("no issues found\n", Color::Green);
     }
     println!("----------------------------------------");
 
