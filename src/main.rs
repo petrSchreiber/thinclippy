51f48a818201dc472fb41743f962b86f63043694
Petr Schreiber <petrschreiber@gmail.com>
2019-08-10 22:29:35 +0200

    refactor: main code made cleaner (#4)
    * refactor: main code made cleaner

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::process::exit;
-use termcolor::Color;
 
 use structopt::StructOpt;
+use termcolor::Color;
 
 mod console;
 mod rules;
@@ -10,7 +10,7 @@ mod tokenizer;
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "thinClippy", about = "Tool for thinBasic code analysis.")]
-struct Opt {
+struct CommandLineParams {
     script_file: String,
 
     #[structopt(short = "w", long = "wait")]
@@ -18,27 +18,30 @@ struct Opt {
 }
 
 fn main() {
-    let command_line_params = Opt::from_args();
+    let command_line_params = CommandLineParams::from_args();
 
-    let main_file_name = command_line_params.script_file;
+    println!("{}", "-".repeat(80));
+    println!("{}", command_line_params.script_file);
+    println!("{}", "-".repeat(80));
 
-    println!("In {}:\n", main_file_name);
-
-    let mut code = match thinbasic_script::Code::new(&main_file_name) {
+    let mut code = match thinbasic_script::Code::new(&command_line_params.script_file) {
         Ok(c) => c,
         Err(e) => {
             console::print_color("input error: ", Color::Red);
             println!("{}", e);
 
-            if command_line_params.wait {
-                console::wait_enter();
-            }
-            exit(1)
+            end_program(1, command_line_params)
         }
     };
 
-    let mut issues_found: i32 = 0;
+    let issues = get_issues(&mut code);
 
+    print_issues(&issues, &mut code);
+
+    end_program((!issues.is_empty()) as i32, command_line_params);
+}
+
+fn get_issues(mut code: &mut thinbasic_script::Code) -> Vec<thinbasic_script::IssueSummary> {
     let mut issues: Vec<thinbasic_script::IssueSummary> = vec![];
 
     let mut compiled_issues = rules::compiled::section_definition(&mut code);
@@ -49,40 +52,45 @@ fn main() {
 
     issues.sort_by(|a, b| a.line.cmp(&b.line));
 
-    for v in issues {
-        print!("\n{}", "-".repeat(80));
+    issues
+}
+
+fn print_issues(issues: &[thinbasic_script::IssueSummary], code: &mut thinbasic_script::Code) {
+    for issue in issues {
+        let mut lines = code.get_file_content().unwrap().lines();
+
+        print!("Line {:>5} - ", issue.line);
+
+        console::print_color(lines.nth((issue.line - 1) as usize).unwrap(), Color::White);
         println!();
 
-        let lines = &mut code.get_file_content().unwrap().lines();
-
-        issues_found += 1;
-
-        print!("Line {:>5} - ", v.line);
-
-        console::print_color(lines.nth((v.line - 1) as usize).unwrap(), Color::White);
-        println!();
-
-        print!("{}", " ".repeat((v.pos + 12) as usize));
+        print!("{}", " ".repeat((issue.pos + 12) as usize));
         println!("^");
         print!("{}", " ".repeat((13) as usize));
-        console::print_color(&v.summary, Color::Red);
+        console::print_color(&issue.summary, Color::Red);
+
+        println!("\n{}", "-".repeat(80));
     }
     println!();
 
-    print!("\n{}", "-".repeat(80));
+    print!("{}", "-".repeat(80));
     print!("\nAnalysis finished: ");
-    if issues_found > 0 {
-        console::print_color(&format!("{}", issues_found), Color::Red);
+
+    if issues.is_empty() {
+        console::print_color(&format!("{}", issues.len()), Color::Red);
         console::print_color(" issue(s) found\n", Color::Red);
     } else {
         console::print_color("no issues found\n", Color::Green);
     }
+
     print!("{}", "-".repeat(80));
     println!();
-    if issues_found > 0 {
-        if command_line_params.wait {
-            console::wait_enter();
-        }
-        exit(2)
+}
+
+fn end_program(exit_code: i32, command_line_params: CommandLineParams) -> ! {
+    if command_line_params.wait {
+        console::wait_enter();
     }
+
+    exit(exit_code)
 }
